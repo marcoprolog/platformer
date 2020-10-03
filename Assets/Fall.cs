b6f5468882ab69b97792f2a95d1165c632a0db9e
Marco Scirea <marco.prolog@gmail.com>
2020-10-03 10:27:45 +0200

    Improved spawner and added platform falling

---
+++ Assets/Fall.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fall : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Invoke("ChangeKinematic", 5);
+        }
+    }
+
+    void ChangeKinematic()
+    {
+        rb.bodyType = RigidbodyType2D.Dynamic;
+    }
+}
--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -6,6 +6,8 @@ public class Spawner : MonoBehaviour
 {
 
     public GameObject platformPrefab;
+    public GameObject player;
+
 
     // Start is called before the first frame update
     void Start()
@@ -13,12 +15,20 @@ public class Spawner : MonoBehaviour
         //spawned as a child of the current object
         //Instantiate(platformPrefab, transform);
         //spawned at specific coordinates
-        Instantiate(platformPrefab, new Vector3(1, 2, 3), Quaternion.identity);
+        //Instantiate(platformPrefab, transform.position, Quaternion.identity);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.x - player.transform.position.x < 70) {
+            //spawn a platform
+            Vector3 offset = Random.Range(-3f, 3) * Vector3.up;
+            Instantiate(platformPrefab, transform.position + offset, Quaternion.identity);
+            //move spawner
+            transform.position += Vector3.right * 20;
+        }
     }
+
+    
 }
